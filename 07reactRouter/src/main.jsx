98fa64d90597c51fc5437b0524fd5d006abe66ca
dnava24 <61266103+dnava24@users.noreply.github.com>
2024-01-18 17:54:17 -0600

    Add Github component and User route

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -11,6 +11,8 @@ import Layout from './Layout.jsx';
 import Home from './components/Home/Home.jsx';
 import About from './components/About/About.jsx';
 import Contact from './components/Contact/Contact.jsx';
+import User from './components/User/User.jsx';
+import Github, {githubInfoLoader} from './components/Github/Github.jsx';
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
@@ -18,6 +20,14 @@ const router = createBrowserRouter(
 			<Route path='' element={<Home />} />
 			<Route path='about' element={<About />} />
 			<Route path='contact' element={<Contact />} />
+			<Route path='user/' element={<User />}>
+				<Route path=':userid' element={<User />} />
+			</Route>
+			<Route
+				loader={githubInfoLoader}
+				path='github'
+				element={<Github />} />
+			<Route path='*' element={<div>Not Found</div>} />
 		</Route>
 	)
 );
